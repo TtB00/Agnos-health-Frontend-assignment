2de2534572051d635e16acec6e3c559d9a531159
TtB00 <88588878+TtB00@users.noreply.github.com>
2024-07-27 22:49:22 +0700

    update abdominal

--- src/containers/Abdominal/index.jsx
+++ src/containers/Abdominal/index.jsx
@@ -2,41 +2,153 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import abdominalPic from "../../assets/default-abs.png";
-import epigastrium from "../../assets/epigastrium-highlight.png";
+import epigastriumHighlight from "../../assets/epigastrium-highlight.png";
+import epigastriumCaption from "../../assets/epigastrium-active.png";
+import luqHighlight from "../../assets/luq-highlight.png";
+import luqCaption from "../../assets/luq-active.png";
+import llqHighlight from "../../assets/llq-highlight.png";
+import llqCaption from "../../assets/llq-active.png";
+import ruqHighlight from "../../assets/ruq-highlight.png";
+import ruqCaption from "../../assets/ruq-active.png";
+import rlqHighlight from "../../assets/rlq-highlight.png";
+import rlqCaption from "../../assets/rlq-active.png";
+import suprapubicHighlight from "../../assets/suprapubic-highlight.png";
+import suprapubicCaption from "../../assets/suprapubic-active.png";
+import umbilicusHighlight from "../../assets/umbilicus-highlight.png";
+import umbilicusCaption from "../../assets/umbilicus-active.png";
 
 function AbdominalArea() {
   const navigate = useNavigate();
-  const [showOverlayEpigastrium, setShowOverlayEpigastrium] = useState(false);
+
+  //button on abdominal
+  const [buttonEpigastrium, setButtonEpigastrium] = useState(true);
+  const [buttonLUQ, setButtonLUQ] = useState(true);
+  const [buttonLLQ, setButtonLLQ] = useState(true);
+  const [buttonRUQ, setButtonRUQ] = useState(true);
+  const [buttonRLQ, setButtonRLQ] = useState(true);
+  const [buttonSuprapubic, setButtonSuprapubic] = useState(true);
+  const [buttonUmbilicus, setButtonUmbilicus] = useState(true);
+
+  //button hide highlight
+  const [buttonHideEpigastrium, setButtonHideEpigastrium] = useState(false);
+  const [buttonHideLUQ, setButtonHideLUQ] = useState(false);
+  const [buttonHideLLQ, setButtonHideLLQ] = useState(false);
+  const [buttonHideRUQ, setButtonHideRUQ] = useState(false);
+  const [buttonHideRLQ, setButtonHideRLQ] = useState(false);
+  const [buttonHideSuprapubic, setButtonHideSuprapubic] = useState(false);
+  const [buttonHideUmbilicus, setButtonHideUmbilicus] = useState(false);
+
+  //Highlight
+  const [highlightEpigastrium, setHighlightEpigastrium] = useState(false);
+  const [highlightLUQ, setHighlightLUQ] = useState(false);
+  const [highlightLLQ, setHighlightLLQ] = useState(false);
+  const [highlightRUQ, setHighlightRUQ] = useState(false);
+  const [highlightRLQ, setHighlightRLQ] = useState(false);
+  const [highlightSuprapubic, setHighlightSuprapubic] = useState(false);
+  const [highlightUmbilicus, setHighlightUmbilicus] = useState(false);
+
+  //Caption
+  const [captionEpigastrium, setCaptionEpigastrium] = useState(false);
+  const [captionLUQ, setCaptionLUQ] = useState(false);
+  const [captionLLQ, setCaptionLLQ] = useState(false);
+  const [captionRUQ, setCaptionRUQ] = useState(false);
+  const [captionRLQ, setCaptionRLQ] = useState(false);
+  const [captionSuprapubic, setCaptionSuprapubic] = useState(false);
+  const [captionUmbilicus, setCaptionUmbilicus] = useState(false);
 
   const nextPage = () => {
     navigate("/finger");
   };
 
+  //Show
   const showEpigastrium = () => {
-    setShowOverlayEpigastrium(true);
+    setButtonEpigastrium(false);
+    setButtonHideEpigastrium(true);
+    setHighlightEpigastrium(true);
+    setCaptionEpigastrium(true);
   };
+  const showLUQ = () => {
+    setButtonLUQ(false);
+    setButtonHideLUQ(true);
+    setHighlightLUQ(true);
+    setCaptionLUQ(true);
+  };
+  const showLLQ = () => {
+    setButtonLLQ(false);
+    setButtonHideLLQ(true);
+    setHighlightLLQ(true);
+    setCaptionLLQ(true);
+  };
+  const showRUQ = () => {
+    setButtonRUQ(false);
+    setButtonHideRUQ(true);
+    setHighlightRUQ(true);
+    setCaptionRUQ(true);
+  };
+  const showRLQ = () => {
+    setButtonRLQ(false);
+    setButtonHideRLQ(true);
+    setHighlightRLQ(true);
+    setCaptionRLQ(true);
+  };
+  const showSuprapubic = () => {
+    setButtonSuprapubic(false);
+    setButtonHideSuprapubic(true);
+    setHighlightSuprapubic(true);
+    setCaptionSuprapubic(true);
+  };
+  const showUmbilicus = () => {
+    setButtonUmbilicus(false);
+    setButtonHideUmbilicus(true);
+    setHighlightUmbilicus(true);
+    setCaptionUmbilicus(true);
+  };
+
+  //Hide
   const hideEpigastrium = () => {
-    setShowOverlayEpigastrium(false);
+    setButtonEpigastrium(true);
+    setButtonHideEpigastrium(false);
+    setHighlightEpigastrium(false);
+    setCaptionEpigastrium(false);
+  };
+  const hideLUQ = () => {
+    setButtonLUQ(true);
+    setButtonHideLUQ(false);
+    setHighlightLUQ(false);
+    setCaptionLUQ(false);
+  };
+  const hideLLQ = () => {
+    setButtonLLQ(true);
+    setButtonHideLLQ(false);
+    setHighlightLLQ(false);
+    setCaptionLLQ(false);
+  };
+  const hideRUQ = () => {
+    setButtonRUQ(true);
+    setButtonHideRUQ(false);
+    setHighlightRUQ(false);
+    setCaptionRUQ(false);
+  };
+  const hideRLQ = () => {
+    setButtonRLQ(true);
+    setButtonHideRLQ(false);
+    setHighlightRLQ(false);
+    setCaptionRLQ(false);
+  };
+  const hideSuprapubic = () => {
+    setButtonSuprapubic(true);
+    setButtonHideSuprapubic(false);
+    setHighlightSuprapubic(false);
+    setCaptionSuprapubic(false);
+  };
+  const hideUmbilicus = () => {
+    setButtonUmbilicus(true);
+    setButtonHideUmbilicus(false);
+    setHighlightUmbilicus(false);
+    setCaptionUmbilicus(false);
   };
 
   return (
-    // <div className="p-14 flex flex-col justify-center items-center ">
-    //   <div class="rounded-2xl border-white shadow-custom flex flex-col mb-4 pb-4 w-96 h-auto">
-    //     <h1 class="text-black text-xl font-bold text-center mt-4">
-    //       จุดไหนที่คุณปวดท้องมากที่สุด ?
-    //     </h1>
-    //     <div class="flex justify-center items-center h-full">
-    //       <img src={abdominalPic} alt="Abdominal" class="h-full object-cover" />
-    //     </div>
-    //   </div>
-    //   <button
-    //     type="button"
-    //     onClick={handleButtonClick}
-    //     class="focus:outline-none text-white text-xl font-bold text-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-2xl px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-96"
-    //   >
-    //     ต่อไป
-    //   </button>
-    // </div>
     <div class="p-14 flex flex-col justify-center items-center min-h-screen bg-gray-100">
       <div class="relative rounded-2xl border-2 border-white shadow-custom flex flex-col mb-4 pb-4 w-96 h-auto bg-white">
         <h1 class="text-black text-xl font-bold text-center mt-4">
@@ -48,28 +160,333 @@ function AbdominalArea() {
             alt="Abdominal"
             class="w-full h-auto object-cover"
           />
-          {showOverlayEpigastrium && (
-            <img
-              src={epigastrium}
-              alt="epigastrium"
-              onClick={hideEpigastrium}
-              class="absolute top-0 left-0 w-24 h-24 object-cover"
-              // style={{ top: "50px", left: "50px" }}
+
+          {/* Button show highlight */}
+          {buttonEpigastrium && (
+            <button
+              type="button"
+              onClick={showEpigastrium}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "40px",
+                height: "40px",
+                top: "160px",
+                left: "165px",
+                zIndex: 1,
+              }}
             />
           )}
-          <button
-            type="button"
-            onClick={showEpigastrium}
-            class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none text-white text-lg font-bold rounded-lg"
-            style={{
-              width: "100px",
-              height: "100px",
-              top: "50px",
-              left: "50px",
-            }}
-          />
+          {buttonLUQ && (
+            <button
+              type="button"
+              onClick={showLUQ}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "35px",
+                height: "45px",
+                top: "190px",
+                left: "205px",
+                zIndex: 1,
+              }}
+            />
+          )}
+          {buttonLLQ && (
+            <button
+              type="button"
+              onClick={showLLQ}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "33px",
+                height: "43px",
+                top: "240px",
+                left: "207px",
+                zIndex: 1,
+              }}
+            />
+          )}
+          {buttonRUQ && (
+            <button
+              type="button"
+              onClick={showRUQ}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "36px",
+                height: "43px",
+                top: "190px",
+                left: "127px",
+                zIndex: 1,
+              }}
+            />
+          )}
+          {buttonRLQ && (
+            <button
+              type="button"
+              onClick={showRLQ}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "36px",
+                height: "43px",
+                top: "240px",
+                left: "127px",
+                zIndex: 1,
+              }}
+            />
+          )}
+          {buttonSuprapubic && (
+            <button
+              type="button"
+              onClick={showSuprapubic}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-lg"
+              class="absolute rounded-full"
+              style={{
+                width: "40px",
+                height: "32px",
+                top: "270px",
+                left: "165px",
+                zIndex: 1,
+              }}
+            />
+          )}
+          {buttonUmbilicus && (
+            <button
+              type="button"
+              onClick={showUmbilicus}
+              // class="absolute bg-transparent border-2 border-dashed border-blue-500 focus:outline-none rounded-full"
+              class="absolute rounded-full"
+              style={{
+                width: "39px",
+                height: "36px",
+                top: "220px",
+                left: "165px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select Epigastrium */}
+          {highlightEpigastrium && (
+            <img
+              src={epigastriumHighlight}
+              alt="epigastrium highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionEpigastrium && (
+            <img
+              src={epigastriumCaption}
+              alt="epigastrium caption"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideEpigastrium && (
+            <button
+              type="button"
+              onClick={hideEpigastrium}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "40px",
+                height: "40px",
+                top: "160px",
+                left: "165px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select luq */}
+          {highlightLUQ && (
+            <img
+              src={luqHighlight}
+              alt="luq highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionLUQ && (
+            <img
+              src={luqCaption}
+              alt="luq caption"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideLUQ && (
+            <button
+              type="button"
+              onClick={hideLUQ}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "35px",
+                height: "45px",
+                top: "190px",
+                left: "205px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select llq */}
+          {highlightLLQ && (
+            <img
+              src={llqHighlight}
+              alt="llq highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionLLQ && (
+            <img
+              src={llqCaption}
+              alt="llq caption"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideLLQ && (
+            <button
+              type="button"
+              onClick={hideLLQ}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "33px",
+                height: "43px",
+                top: "240px",
+                left: "207px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select ruq */}
+          {highlightRUQ && (
+            <img
+              src={ruqHighlight}
+              alt="ruq highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionRUQ && (
+            <img
+              src={ruqCaption}
+              alt="ruq caption"
+              class="absolute top-8 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideRUQ && (
+            <button
+              type="button"
+              onClick={hideRUQ}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "36px",
+                height: "43px",
+                top: "190px",
+                left: "127px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select rlq */}
+          {highlightRLQ && (
+            <img
+              src={rlqHighlight}
+              alt="rlq highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionRLQ && (
+            <img
+              src={rlqCaption}
+              alt="ruq caption"
+              class="absolute bottom-3 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideRLQ && (
+            <button
+              type="button"
+              onClick={hideRLQ}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "36px",
+                height: "43px",
+                top: "240px",
+                left: "127px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select Suprapubic */}
+          {highlightSuprapubic && (
+            <img
+              src={suprapubicHighlight}
+              alt="rlq highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionSuprapubic && (
+            <img
+              src={suprapubicCaption}
+              alt="ruq caption"
+              class="absolute top-5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideSuprapubic && (
+            <button
+              type="button"
+              onClick={hideSuprapubic}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-lg"
+              class="absolute rounded-lg"
+              style={{
+                width: "40px",
+                height: "32px",
+                top: "270px",
+                left: "165px",
+                zIndex: 1,
+              }}
+            />
+          )}
+
+          {/* Select Umbilicus */}
+          {highlightUmbilicus && (
+            <img
+              src={umbilicusHighlight}
+              alt="umbilicus highlight"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {captionUmbilicus && (
+            <img
+              src={umbilicusCaption}
+              alt="umbilicus caption"
+              class="absolute top-4.5 left-7.5 w-88 object-cover"
+            />
+          )}
+          {buttonHideUmbilicus && (
+            <button
+              type="button"
+              onClick={hideUmbilicus}
+              // class="absolute bg-transparent border-2 border-dashed border-red-500 focus:outline-none rounded-full"
+              class="absolute rounded-full"
+              style={{
+                width: "39px",
+                height: "36px",
+                top: "220px",
+                left: "165px",
+                zIndex: 1,
+              }}
+            />
+          )}
         </div>
       </div>
+
       <button
         type="button"
         onClick={nextPage}

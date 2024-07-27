49cbda5d9fb5f0a122e19bc51c7b901cb6a1ba8c
TtB00 <88588878+TtB00@users.noreply.github.com>
2024-07-27 16:19:47 +0700

    update

--- src/containers/Abdominal/index.jsx
+++ src/containers/Abdominal/index.jsx
@@ -3,29 +3,26 @@ import abdominalPic from "../../assets/default-abs.png";
 
 function AbdominalArea() {
   return (
-    <div className="p-24">
-      {/* <div class="relative h-200 w-200 bg-green-300 p-8">
-        <p>Relative parent</p>
-        <div class="absolute h-50 w-50 bottom-5 left-20 bg-red-700 ">
-          <p>Absolute child</p>
-        </div>
-      </div> */}
-      {/* <h1 class="text-black">Abdominal</h1> */}
+    <div className="p-24 flex justify-center items-center">
       <div>
-        <div>
-          <h1 class="text-black">จุดไหนที่คุณปวดท้องมากที่สุด</h1>
-          <div></div>
+        <div class="rounded-2xl border-2 border-white shadow-custom mb-4 w-96 h-auto">
+          <h1 class="text-black text-xl font-bold text-center mt-4">
+            จุดไหนที่คุณปวดท้องมากที่สุด ?
+          </h1>
+          <div class="flex justify-center items-center h-full">
+            <img
+              src={abdominalPic}
+              alt="Abdominal"
+              class="h-full object-cover"
+            />
+          </div>
         </div>
         <button
           type="button"
-          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          class="focus:outline-none text-white text-xl font-bold text-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 w-96"
         >
           ต่อไป
         </button>
-      </div>
-
-      <div>
-        <img src={abdominalPic} alt="Abdominal" />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,18 @@ export default {
       "50%": "50%",
       16: "4rem",
     },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    boxShadow: {
+      custom: "0 0 10px rgba(0, 0, 0, 0.5)",
+    },
   },
   plugins: [],
 };
